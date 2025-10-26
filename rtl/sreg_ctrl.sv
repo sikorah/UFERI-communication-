93549197f30e47abf4e9193a9388a90b23adaeb4
sikorah <sikora.hubert@outlook.com>
2025-10-26 19:46:41 +0100

    Initial rewrite

--- rtl/sreg_ctrl.sv
+++ rtl/sreg_ctrl.sv
@@ -1,11 +1,13 @@
 `timescale 1ns/1ps
 
 module sreg_ctrl (
-    input logic sclk,
+    input logic clk,
     input logic rst_n,
     input logic [41:0] data_in,
 
     output logic ready,
+    output logic shift,
+    output logic sclk,
     output logic [41:0] data_out
 );
 
@@ -13,18 +15,32 @@ typedef enum {IDLE, LOAD_SREG, READ_SREG} op_type_t;
 
 op_type_t state;
 
-always_ff @(posedge sclk) begin
-    if (!rst_n) begin
-        ready <= 1'b0;
-        data_out <= 42'd0;
+logic [5:0] shift_cnt;
+logic [1:0] clk_div;
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if(rst_n) begin
         state <= IDLE;
+        data_out <= 42'd0;
+        clk_div <= 2'd0;
+        slck <= 1'b0;
+        ready <= 1'b1;
+        shift <= 1'b0;
+        shift_cnt <= 6'd0;
     end else begin
-        case(state)
+        clk_div <= clk_div + 2'd1;
+        sclk <= clk_div[1];
+
+        ready <= 1'b0;
+        shift <= 1'b0;
+
+        case (state)
             IDLE: begin
+                shift_cnt <= 6'd0;
                 if (ready) begin
                     state <= LOAD_SREG;
                 end else begin
-                    state <= IDLE;
+                    state <= IDLE:
                 end
             end
             LOAD_SREG: begin
@@ -32,8 +48,17 @@ always_ff @(posedge sclk) begin
                 state <= READ_SREG;
             end
             READ_SREG: begin
-                data_out <= {2'b00, data_out[41:2]};
-                state <= IDLE;
+                shift <= 1'b1;
+
+                data_out <= {1'b0, data_out[41:1]};
+
+                if(shift_cnt == 6'd41) begin
+                    state <= IDLE;
+                    shift_cnt <= 6'd0;
+                end else begin
+                    shift_cnt <= shift_cnt + 6'd1;
+                    state <= READ_SREG
+                end
             end
             default: begin
                 state <= IDLE;
--- rtl/sreg_model.sv
+++ rtl/sreg_model.sv
@@ -5,26 +5,34 @@ module sreg_model(
     input logic rst_n,
     input logic shift,
     input logic load,
-    input logic [41:0] pix_in,
+    input logic serial_in,
+    input logic [41:0] pixel_in,
 
     output logic [1:0] sreg_out
 );
 
-logic [41:0] sreg;
+logic [41:0] sreg, next_sreg;
 
-always_ff @(posedge sclk) begin
-    if (!rst_n) begin
-        sreg_out <= 2'b00;
-        sreg <= 42'd0;
-    end else if (load)begin
-        sreg <= pix_in;
-
+always_comb begin
+    next_sreg = sreg;
+    if (load) begin
+        next_sreg = pixel_in;
     end else if (shift) begin
-        sreg <= {2'b00, sreg[41:2]};
-    end else begin
-        sreg_out <= sreg[1:0];
+        next_sreg = {sdin, sreg[41:1]};
     end
 end
+
+always_ff @(posedge sclk or negedge rst_n) begin
+    if (!rst_n) begin
+        sreg <= 42'd0;
+        sreg_out <= 2'b00;
+    end else begin
+        sreg <= next_sreg;
+        sreg_out <= next_sreg[20];
+        sreg_out <= next_sreg[41];
+    end
+end
+
 
 
 endmodule
