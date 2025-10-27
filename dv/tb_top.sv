f191dae0edd5fe1f25b17fdd9b072e40e34496ef
sikorah <sikora.hubert@outlook.com>
2025-10-27 18:08:24 +0100

    Control sim success

--- dv/tb_top.sv
+++ dv/tb_top.sv
@@ -1,27 +1,43 @@
 `timescale 1ns/1ps
 
 module tb_top();
+logic clk;
 logic sclk;
 logic rst_n;
 logic shift;
+logic ready;
+
 logic serial_in;
 logic [41:0] pixel_in;
+logic [41:0] data_in;
+logic [1:0] sreg_in;
+
 logic [1:0] sreg_out;
+logic [41:0] data_out;
+
+sreg_ctrl u_sreg_ctrl(
+    .clk,
+    .rst_n,
+    .data_in,
+    .sreg_in,
+    .ready,
+    .shift,
+    .data_out
+);
 
 sreg_model u_sreg_model(
-    .sclk,
+    .sclk(clk),
     .shift,
     .serial_in,
-    .pixel_in,
+    .pixel_in(data_out),
     .sreg_out
 );
 
-
 initial begin
-    sclk = '0;
+    clk = '0;
 
     forever begin
-        #2.5 sclk = ~sclk;
+        #2.5 clk = ~clk;
     end
 end
 
@@ -29,36 +45,55 @@ initial begin
     rst_n = '0;
 
     repeat (42)
-        @(posedge sclk);
+        @(posedge clk);
 
     rst_n = '1;
 end
 
 task test_sreg_model();
-    logic [41:0] test_data = 42'hAAA_BBBB_CCCC;
+    logic [41:0] test_data = 42'b10_0110_1011_0100_1011_0100_1111_0110_1001_0010_1010;
     shift <= '0;
 
     pixel_in <= test_data;
-    @(posedge sclk);
+    @(posedge clk);
 
     shift <= '1;
     repeat(42) begin
-        @(posedge sclk);
+        @(posedge clk);
     end
 
     shift <= '0;
 endtask
 
+task test_sreg_control();
+    logic [41:0] test_data = 42'b10_0110_1011_0100_1011_0100_1111_0110_1001_0010_1010;
+    shift <= '0;
+    ready <= '0;
+
+    data_in <= test_data;
+    @(posedge clk);
+
+    ready <= '1;
+    repeat(42) begin
+        @(posedge clk);
+    end
+
+    shift <= '0;
+    ready <= '0;
+endtask
+
 initial begin
     shift <= '0;
+    serial_in <= '0;
+    ready <= '0;
     
     repeat (8)
-        @(posedge sclk);
+        @(posedge clk);
 
-    test_sreg_model();
+    test_sreg_control();
 
     repeat (8)
-        @(posedge sclk);
+        @(posedge clk);
 
     $finish;
 
--- rtl/sreg_ctrl.sv
+++ rtl/sreg_ctrl.sv
@@ -4,10 +4,10 @@ module sreg_ctrl (
     input logic clk,
     input logic rst_n,
     input logic [41:0] data_in,
+    input logic [1:0] sreg_in,
 
     output logic ready,
     output logic shift,
-    output logic sclk,
     output logic [41:0] data_out
 );
 
@@ -16,28 +16,16 @@ typedef enum {IDLE, LOAD_SREG, READ_SREG} op_type_t;
 op_type_t state;
 
 logic [5:0] shift_cnt;
-logic [1:0] clk_div;
 
 always_ff @(posedge clk or negedge rst_n) begin
     if(rst_n) begin
-        state <= IDLE;
-        data_out <= 42'd0;
-        clk_div <= 2'd0;
-        sclk <= 1'b0;
-        ready <= 1'b1;
-        shift <= 1'b0;
-        shift_cnt <= 6'd0;
+        ready <= '0;
+        shift <= '0;
+        data_out <= '0;
     end else begin
-        clk_div <= clk_div + 2'd1;
-        sclk <= clk_div[1];
-
-        ready <= 1'b0;
-        shift <= 1'b0;
-
         case (state)
             IDLE: begin
-                shift_cnt <= 6'd0;
-                if (ready) begin
+                if(ready) begin
                     state <= LOAD_SREG;
                 end else begin
                     state <= IDLE;
@@ -48,21 +36,15 @@ always_ff @(posedge clk or negedge rst_n) begin
                 state <= READ_SREG;
             end
             READ_SREG: begin
-                shift <= 1'b1;
-
-                data_out <= {1'b0, data_out[41:1]};
-
-                if(shift_cnt == 6'd41) begin
-                    state <= IDLE;
+                shift = 1'b1;
+                if (shift_cnt == 6'd41) begin
                     shift_cnt <= 6'd0;
+                    state <= IDLE;
                 end else begin
-                    shift_cnt <= shift_cnt + 6'd1;
+                    shift_cnt = shift_cnt + 1;
                     state <= READ_SREG;
                 end
-            end
-            default: begin
-                state <= IDLE;
-            end
+            end 
         endcase
     end
 end
