cb38208700f6e7376161a5c42fed929fcc2472fe
sikorah <sikora.hubert@outlook.com>
2025-11-16 19:40:16 +0100

    Control commands

--- dv/tb_top.sv
+++ dv/tb_top.sv
@@ -1,154 +1,123 @@
 `timescale 1ns/1ps
 
-module tb_top();
+module tb_top;
+// sygnały kontrolera
 logic clk;
-logic sclk;
 logic rst_n;
+logic cmd_valid;
+logic [2:0] cmd;
+logic [41:0] data_in;
+logic [41:0] data_out;
+logic cmd_ready;
+logic [1:0] sreg_in;
 logic shift;
-//logic ready;
+logic sclk;
+logic serial_out;
+logic write_cfg;
 
+// sygnały modelu
 logic serial_in;
 logic [41:0] pixel_in;
-//logic [41:0] data_in;
-//logic [1:0] sreg_in;
-
-//logic serial_out;
 logic [1:0] sreg_out;
-//logic [41:0] data_out;
-
-logic write_cfg;
 logic [41:0] cfg_out;
 
+sreg_ctrl u_sreg_ctrl(
+    .clk        (clk),
+    .rst_n      (rst_n),
+    .cmd_valid  (cmd_valid),
+    .cmd        (cmd),
+    .data_in    (data_in),
+    .data_out   (data_out),
+    .cmd_ready  (cmd_ready),
+    .sreg_in    (sreg_in),
+    .shift      (shift),
+    .sclk       (sclk),
+    .serial_out (serial_out),
+    .write_cfg  (write_cfg)
+);
+    
 sreg_model u_sreg_model(
-    .sclk,
-    .shift,
-    .serial_in,
-    .pixel_in,
-    .sreg_out,
-    .write_cfg,
-    .cfg_out
+    .sclk       (sclk),
+    .shift      (shift),
+    .serial_in  (serial_out),
+    .write_cfg  (write_cfg),
+    .pixel_in   (data_in),
+    .sreg_out   (sreg_in),
+    .cfg_out    (cfg_out)
 );
 
-/*sreg_ctrl u_sreg_ctrl(
-    .clk,
-    .rst_n,
-    .data_in,
-    .sreg_in(sreg_out),
-    .ready,
-    .shift(shift),
-    .sclk(sclk),
-    .serial_out(serial_in),
-    .data_out
-);*/
+// generacja zegara i resetu
 
 initial begin
     clk = '0;
-
-    forever begin
-        #2.5 clk = ~clk;
-    end
-end
-
-initial begin
     rst_n = '0;
 
-    repeat (84)
+    forever begin
+        #5 clk = ~clk;
+    end
+
+    repeat(84)
         @(posedge clk);
 
     rst_n = '1;
 end
 
-task write_sreg(input logic [41:0] test_data);
-
+task send_command(input logic [2:0] command, input logic [41:0] data);
     @(posedge clk);
-    sclk <= '1;
-    for (int i = 0; i<84; ++i) begin
-        @(posedge clk);
-        sclk <= ~sclk;
-        if(sclk == 1) begin
-            shift <= '1;
-            serial_in <= test_data[41];
-            test_data <= test_data << 1;
-        end
-    end
-
- /*   pixel_in <= test_data;
+    cmd <= command;
+    data_in <= data;
+    cmd_valid <= 1'b1;
     @(posedge clk);
-
-    shift <= '1;
-    repeat(21) begin
-        @(posedge clk);
-    end
-
-    shift <= '0;
-    pixel_in <= 42'b0;
-    @(posedge clk);
-
-    // SIPO Test
-    shift <= '1;
-    repeat(42) begin
-        serial_in <= test_data[41:40];
-        test_data <= test_data << 1;
-        @(posedge clk);
-    end
-    shift <= '0;*/
+    cmd_valid <= 1'b0;
 endtask
-
-task write_config(input logic [41:0] test_data);
-
-    @(posedge clk);
-    sclk <= '1;
-    for (int i = 0; i<84; ++i) begin
-        @(posedge clk);
-        sclk <= ~sclk;
-        if(sclk == 1) begin
-            shift <= '1;
-            serial_in <= test_data[41];
-            test_data <= test_data << 1;
-            write_cfg <= i == 82 ? 1'b1 : 1'b0;
-        end
-    end
-    @(posedge clk);
-    write_cfg <= 0;
-
-    endtask
-
-/*task test_sreg_control();
-    logic [41:0] test_data = 42'b10_0110_1011_0100_1011_0101_1111_0110_1001_0010_1011;
-    ready <= '0;
-
-    data_in <= test_data;
-    @(posedge clk);
-
-    ready <= '1;
-    repeat(42) begin
-        @(posedge clk);
-    end
-
-    ready <= '0;
-endtask*/
 
 initial begin
     logic [41:0] test_data1 = 42'b10_0110_1011_0100_1011_0101_1111_0110_1001_0010_1011;
     logic [41:0] test_data2 = ~(42'b10_0110_1011_0110_1011_0100_1111_0110_1001_0010_1011);
+
     shift <= '0;
     serial_in <= '0;
     sclk <= '1;
-    
-    repeat (4)
+
+    repeat (2)
         @(posedge clk);
 
-    write_sreg(test_data1);
-    write_config(test_data2);
+    send_command(3'b000, test_data1); // PIX_WRITE
+    repeat (2)
+        @(posedge clk);
 
-    repeat (4)
+    send_command(3'b001, '0); // PIX_READ
+    repeat (2)
+        @(posedge clk);
+
+    send_command(3'b010, '0); // PIX_READ_END
+    repeat (2)
+        @(posedge clk);
+
+    send_command(3'b011, test_data1); // WRITE_PCLK_0
+    repeat (2)
+        @(posedge clk);
+
+    send_command(3'b100, test_data1); // WRITE_PCLK_1
+    repeat (2)
+        @(posedge clk);
+
+    send_command(3'b101, test_data2); // WRITE_FULL_PCLK_0
+    repeat (2)
+        @(posedge clk);
+
+    send_command(3'b110, test_data2); // WRITE_FULL_PCLK_1
+    repeat (2)
+        @(posedge clk);
+
+    send_command(3'b111, '0); // SREG_READ
+    repeat (2)
         @(posedge clk);
 
     shift <= '0;
     serial_in <= '0;
     sclk <= '1;
     $finish;
-
 end
 
 endmodule
--- rtl/sreg_ctrl.sv
+++ rtl/sreg_ctrl.sv
@@ -18,88 +18,236 @@ module sreg_ctrl (
     output logic shift,
     output logic sclk,
     output logic serial_out,
-    output logic write_cfg,
+    output logic write_cfg
 );
 
-typedef enum {IDLE, LOAD_SREG, READ_SREG} op_type_t;
+reg [41:0]  data_int;
 
+// liczniki 
+logic [5:0] counter_int;
+logic [5:0] counter_limit;
+
+typedef enum {IDLE, LOAD_SREG, READ_SREG} op_type_t;
 op_type_t state;
 
+typedef enum logic [2:0] {
+    PIX_WRITE          = 3'b000,
+    PIX_READ           = 3'b001,
+    PIX_READ_END       = 3'b010,
+    WRITE_PCLK_0       = 3'b011,
+    WRITE_PCLK_1       = 3'b100,
+    WRITE_FULL_PCLK_0  = 3'b101,
+    WRITE_FULL_PCLK_1  = 3'b110,
+    SREG_READ          = 3'b111
+} cmd_opcode_t;
+
 always_ff @(posedge clk) begin
-    if(!rst_n) begin
-        shift <= '0;
-        data_out <= '0;
-        sclk <= '0;
-        serial_out <= '0;
+    if (!rst_n) begin
+        data_out <= 42'b0;
+        cmd_ready <= 1'b0;
+        shift <= 1'b0;
+        sclk <= 1'b1;
+        serial_out <= 1'b0;
+        write_cfg <= 1'b0;
+        counter_int <= 6'b0;
+        counter_limit <= 6'b0;
         state <= IDLE;
     end else begin
+        data_int <= data_in;
         case (state)
             IDLE: begin
                 sclk <= sclk;
                 shift <= shift;
+                counter_limit <= 6'b0;
                 if (cmd_valid && cmd_ready) begin
-                    cmd_ready <= '0;
-                    case(cmd) 
-                       0: begin
-                        serial_out <= 42'b0;
-                        shift <= '1;
-                        write_cfg <= '0;
-                        state <= READ_SREG;
-                       end 
-                       1: begin
-                        serial_out <= 42'b0;
-                        shift <= '1;
-                        write_cfg <= '0;
-                        state <= LOAD_SREG;
-                       end
-                       2: begin
-                        shift <= '1;
-                        write_cfg <= '0;
-                        state <= READ_SREG;
-                       end
-                       3: begin
-                        shift <= '1;
-                        write_cfg <= '1;
-                        state <= LOAD_SREG;
-                       end
-                       4: begin
-                        shift <= '1;
-                        write_cfg <= '1;
-                        state <= READ_SREG;
-                       end
-                       5: begin
-                        state <= LOAD_SREG;
-                       end
+                    cmd_ready <= 1'b0;
+                    case(cmd)
+                        PIX_WRITE: begin
+                            counter_limit <= 6'd84;
+                            state <= LOAD_SREG;
+                        end
+                        PIX_READ: begin
+                            counter_limit <= 6'd42;
+                            state <= READ_SREG;
+                        end
+                        PIX_READ_END: begin
+                            counter_limit <= 6'd6;
+                            state <= READ_SREG;
+                        end
+                        WRITE_PCLK_0: begin
+                            counter_limit <= 6'd20;
+                            state <= LOAD_SREG;
+                        end
+                        WRITE_PCLK_1: begin
+                            counter_limit <= 6'd20;
+                            state <= LOAD_SREG;
+                        end
+                        WRITE_FULL_PCLK_0: begin
+                            counter_limit <= 6'd84;
+                            state <= LOAD_SREG;
+                        end
+                        WRITE_FULL_PCLK_1: begin
+                            counter_limit <= 6'd84;
+                            state <= LOAD_SREG;
+                        end
+                        SREG_READ: begin
+                            counter_limit <= 6'd46;
+                            state <= READ_SREG;
+                        end
+                        default: begin
+                            state <= IDLE;
+                        end
                     endcase
-                end else begin
-                    cmd_ready <= '1;
+                end else begin 
+                    cmd_ready <= 1'b1;
                     state <= IDLE;
                 end
             end
-            LOAD_SREG: begin
+            LOAD_SREG: begin 
                 sclk <= ~sclk;
-                shift <= '1;
-                if (ready) begin
-                    serial_out <= data_in[41];
-                    data_in <= data_in << 1;
-                    state <= READ_SREG;
-                end else begin
-                    state <= IDLE;
-                end
+                case(cmd)
+                    PIX_WRITE: begin
+                        shift <= 1'b1;
+                        write_cfg <= 1'b0;
+                        //dvalid_out <= 1'b0;
+                        if (counter_int < counter_limit) begin
+                            serial_out <= data_int[41];
+                            data_int <= data_int << 1;
+                            counter_int <= counter_int + 1;
+                            state <= LOAD_SREG;
+                        end else begin
+                            shift <= 1'b0;
+                            write_cfg <= 1'b0;
+                            counter_int <= 6'b0;
+                            state <= IDLE;
+                        end
+                    end
+                    WRITE_PCLK_0: begin
+                        shift <= 1'b1;
+                        //dvalid_out <= 1'b0;
+                        if (counter_int < counter_limit) begin
+                            serial_out <= data_int[41];
+                            data_int <= data_int << 1;
+                            counter_int <= counter_int + 1;
+                            state <= LOAD_SREG;
+                        end else if(counter_int == 18) begin
+                            write_cfg <= 1'b1;
+                            state <= LOAD_SREG;
+                        end else begin
+                            shift <= 1'b0;
+                            write_cfg <= 1'b0;
+                            counter_int <= 6'b0;
+                            state <= IDLE;
+                        end
+                    end
+                    WRITE_PCLK_1: begin
+                        shift <= 1'b1;
+                        //dvalid_out <= 1'b0;
+                        if (counter_int < counter_limit) begin
+                            serial_out <= data_int[41];
+                            data_int <= data_int << 1;
+                            counter_int <= counter_int + 1;
+                            state <= LOAD_SREG;
+                        end else if(counter_int == 18) begin
+                            write_cfg <= 1'b1;
+                            state <= LOAD_SREG;
+                        end else begin
+                            shift <= 1'b0;
+                            write_cfg <= 1'b0;
+                            counter_int <= 6'b0;
+                            state <= IDLE;
+                        end
+                    end
+                    WRITE_FULL_PCLK_0: begin
+                        shift <= 1'b1;
+                        //dvalid_out <= 1'b0;
+                        if (counter_int < counter_limit) begin
+                            serial_out <= data_int[41];
+                            data_int <= data_int << 1;
+                            counter_int <= counter_int + 1;
+                            state <= LOAD_SREG;
+                        end else if(counter_int == 82) begin
+                            write_cfg <= 1'b1;
+                            state <= LOAD_SREG;
+                        end else begin
+                            shift <= 1'b0;
+                            write_cfg <= 1'b0;
+                            counter_int <= 6'b0;
+                            state <= IDLE;
+                        end
+                    end
+                    WRITE_FULL_PCLK_1: begin
+                        shift <= 1'b1;
+                        //dvalid_out <= 1'b0;
+                        if (counter_int < counter_limit) begin
+                            serial_out <= data_int[41];
+                            data_int <= data_int << 1;
+                            counter_int <= counter_int + 1;
+                            state <= LOAD_SREG;
+                        end else if(counter_int == 82) begin
+                            write_cfg <= 1'b1;
+                            state <= LOAD_SREG;
+                        end else begin
+                            shift <= 1'b0;
+                            write_cfg <= 1'b0;
+                            counter_int <= 6'b0;
+                            state <= IDLE;
+                        end
+                    end
+                endcase
             end
-            READ_SREG: begin
+            READ_SREG: begin 
                 sclk <= ~sclk;
-                shift <= '1;
-                if (ready) begin
-                    data_out[41:21] <= {'0, sreg_in[0]};
-                    data_out[20:0] <= {'0, sreg_in[1]};
-                    state <= LOAD_SREG;
-                end else begin
-                    state <= IDLE;
-                end
+                case(cmd)
+                    PIX_READ: begin
+                        write_cfg <= 1'b0;
+                        //dvalid_out <= 1'b1;
+                        if (counter_int < counter_limit) begin
+                            data_out[41:21] <= {'0, sreg_in[0]};
+                            data_out[20:0] <= {'0, sreg_in[1]};
+                            counter_int <= counter_int + 1;
+                            state <= READ_SREG;
+                        end else if (counter_int == 2) begin
+                            shift <= 1'b1;
+                        end else begin
+                            shift <= 1'b0;
+                            counter_int <= 6'b0;
+                            state <= IDLE;
+                        end
+                    end
+                    PIX_READ_END: begin
+                        write_cfg <= 1'b0;
+                        //dvalid_out <= 1'b0;
+                        if (counter_int < counter_limit) begin
+                            shift <= 1'b1;
+                            counter_int <= counter_int + 1;
+                            state <= READ_SREG;
+                        end else begin
+                            shift <= 1'b0;
+                            counter_int <= 6'b0;
+                            state <= IDLE;
+                        end
+                    end
+                    SREG_READ: begin
+                        shift <= 1'b1;
+                        write_cfg <= 1'b0;
+                        if (counter_int < counter_limit) begin
+                            data_out[41:21] <= {'0, sreg_in[0]};
+                            data_out[20:0] <= {'0, sreg_in[1]};
+                            counter_int <= counter_int + 1;
+                            state <= READ_SREG;
+                        //end else if (counter_int > 0 && counter_int < 42) begin
+                            //dvalid_out <= 1'b1;
+                        end else begin
+                            shift <= 1'b0;
+                            counter_int <= 6'b0;
+                            state <= IDLE;
+                        end
+                    end
+                endcase
             end
         endcase
     end
-end
+end 
 
 endmodule
